f859a25bd5c69b5bdd10a94bdcee2957b021ec76
Rione <anderson020398.007@gmail.com>
2025-11-21 05:24:51 -0500

    agregado commands

---
+++ Backend_Geo_Incidencia.Application/Features/Incidencia/Commands/BuscarArea/BuscarAreaCommand.cs
@@ -0,0 +1,29 @@
+﻿using Backend_Geo_Incidencia.Application.Features.Incidencia.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Geo_Incidencia.Application.Features.Incidencia.Commands.BuscarArea
+{
+    public class BuscarAreaCommand: IRequest<BuscarAreaResponse>
+    {
+        public decimal MinLat { get; set; }
+        public decimal MaxLat { get; set; }
+        public decimal MinLng { get; set; }
+        public decimal MaxLng { get; set; }
+        public List<int>? Tipos { get; set; }
+        public List<int>? Subtipos { get; set; }
+        public int? Dias { get; set; }
+
+    }
+    public class BuscarAreaResponse
+    {
+        public List<IncidenciaDto> Incidencias { get; set; } = [];
+        public string? Mensaje { get; set; }
+        public int CodigoRespuesta { get; set; }
+        public bool Exito => CodigoRespuesta == 0;
+    }
+}
--- Backend_Geo_Incidencia.Application/Features/Incidencia/Dtos/IncidenciaDto.cs
+++ Backend_Geo_Incidencia.Application/Features/Incidencia/Dtos/IncidenciaDto.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Backend_Geo_Incidencia.Domain.Entities
+namespace Backend_Geo_Incidencia.Application.Features.Incidencia.Dtos
 {
-    public class IncidenciaEntity
+    public class IncidenciaDto
     {
         public int ID_INCIDENCIA { get; set; }
         public int ID_TIPO { get; set; }
@@ -18,7 +18,7 @@ namespace Backend_Geo_Incidencia.Domain.Entities
         public decimal LONGITUD { get; set; }
         public string? DESCRIPCION { get; set; }
         public string? FOTO_URL { get; set; }
-        public string? DIRECCION_REFERENCIA { get;set; }
+        public string? DIRECCION_REFERENCIA { get; set; }
         public bool ESTADO { get; set; }
         public string? DISTANCIA { get; set; } // Usado solo en busqueda por radio
     }
